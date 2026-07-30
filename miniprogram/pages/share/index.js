686cc54ac606996833df2cb8a93ff80dbf9a0024
zhangtianrun0522-pixel <zhangtianrun0522@gmail.com>
2026-07-31 00:03:48 +0800

    fix(share): show explicit loading and unavailable states

--- miniprogram/pages/share/index.js
+++ miniprogram/pages/share/index.js
@@ -2,44 +2,53 @@ var analytics = require('../../utils/analytics');
 var cloudBase = require('../../utils/cloudBase');
 var tabNav = require('../../utils/tabNav');
 
-function fallbackPayload() {
-  return {
-    title: '梦卡',
-    cardNo: 'NO. 001',
-    cardTheme: 'mist',
-    profileSummary: 'ONEIRO',
-    emotionalWeather: '一张梦卡正在路上。',
-    symbols: ['梦'],
-    cardInsight: '写下你的梦，也可以抽出属于自己的牌。',
-  };
+function isInvalidShareReason(reason) {
+  return /share_not_found|not_found|expired|revoked/.test(String(reason || '').toLowerCase());
 }
 
 Page({
   data: {
     loading: true,
     shareId: '',
-    payload: fallbackPayload()
+    payload: {},
+    errorState: ''
   },
 
   onLoad: function (options) {
-    var shareId = options && options.id ? decodeURIComponent(options.id) : '';
-    var that = this;
+    var shareId = '';
+
+    try {
+      shareId = options && options.id ? decodeURIComponent(options.id) : '';
+    } catch (error) {
+      shareId = '';
+    }
 
     this.setData({ shareId: shareId });
     analytics.trackEvent('share_card_open', { hasShareId: !!shareId });
 
+    this.loadShareCard();
+  },
+
+  loadShareCard: function () {
+    var that = this;
+    var shareId = this.data.shareId;
+
+    this.setData({ loading: true, errorState: '' });
+
     cloudBase.getShareCard(shareId, function (res) {
       if (res && res.ok && res.payload) {
-        that.setData({ loading: false, payload: res.payload });
+        that.setData({ loading: false, payload: res.payload, errorState: '' });
         analytics.trackEvent('share_card_loaded', {
           cardTheme: res.payload.cardTheme || 'mist'
         });
         return;
       }
 
-      that.setData({ loading: false, payload: fallbackPayload() });
+      var reason = res && res.reason ? String(res.reason) : 'share_card_load_failed';
+      var errorState = isInvalidShareReason(reason) ? 'not_found' : 'unavailable';
+      that.setData({ loading: false, payload: {}, errorState: errorState });
       analytics.trackEvent('share_card_load_failed', {
-        reason: res && res.reason ? res.reason : 'local_fallback'
+        reason: reason
       });
     });
   },
